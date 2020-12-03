41900feca0ddfd6efd7eabfcc0ee4febcbce12f9
vinicius-barbosa <vinicius.barbosa.nw@gmail.com>
2020-12-03 14:20:45 -0400

    Incluindo classes resposáveis pelos cálculos de decomposição.

---
+++ CalculoDeDivisoresWebSevice/CalculoDeDivisores/GeracaoDeDivisoresUtil.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculoDeDivisores
+{
+	public class GeracaoDeDivisoresUtil
+	{
+		public static List<int> ObterDivisoresDoNumero(int numero, bool apenasDivisoresPrimos)
+		{
+			var divisores = new List<int>() { 1 };
+
+			var listaAuxiliar = new List<int>() { 1 };
+
+			var decomposicaoEmFatoresPrimos = ObterDecomposicaoEmFatoresPrimos(numero);
+			var divisoresPrimos = decomposicaoEmFatoresPrimos.Where(d => d.Key != 1).Select(d => d.Value.Value).ToList();
+
+			if(apenasDivisoresPrimos)
+			{
+				return divisoresPrimos;
+			}
+			else
+			{
+				foreach (int divisorPrimo in divisoresPrimos)
+				{
+					listaAuxiliar.Clear();
+
+					foreach (int divisor in divisores)
+					{
+						var produto = divisorPrimo * divisor;
+
+						if (!divisores.Contains(produto))
+						{
+							listaAuxiliar.Add(produto);
+						}
+					}
+
+					divisores.AddRange(listaAuxiliar);
+				}
+
+				divisores.Sort();
+
+				return divisores;
+			}
+		}
+
+		private static Dictionary<int, int?> ObterDecomposicaoEmFatoresPrimos(int numero)
+		{
+			int divisorParaRecursividade = 7;
+			var decomposicaoCompleta = new Dictionary<int, int?>();
+
+			if (NumeroPrimoUtil.NumeroEhPrimo(numero, divisorParaRecursividade))
+			{
+				decomposicaoCompleta.Add(numero, numero);
+				decomposicaoCompleta.Add(1, null);
+			}
+			else
+			{
+				PreencherDecomposicao(decomposicaoCompleta, numero);
+				decomposicaoCompleta.Add(1, null);
+			}
+
+			return decomposicaoCompleta;
+		}
+
+		private static void PreencherDecomposicao(Dictionary<int, int?> decomposicaoCompleta, int numero)
+		{
+			int divisorAtual = 2;
+			do
+			{
+				if (NumeroPrimoUtil.EhDivisivelPorAlgumDosDivisores(numero, divisorAtual))
+				{
+					decomposicaoCompleta.Add(numero, divisorAtual);
+					int proximoDividendo = (numero / divisorAtual);
+					PreencherDecomposicao(decomposicaoCompleta, proximoDividendo);
+				}
+				else if (numero > 1)
+				{
+					divisorAtual = NumeroPrimoUtil.ObterProximoNumeroPrimo(divisorAtual);
+				}
+
+			} while ((decomposicaoCompleta.Count == 0) || (decomposicaoCompleta.Last().Key != decomposicaoCompleta.Last().Value));
+		}
+	}
+}
---
+++ CalculoDeDivisoresWebSevice/CalculoDeDivisores/NumeroPrimoUtil.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CalculoDeDivisores
+{
+	public static class NumeroPrimoUtil
+	{
+		public static bool NumeroEhPrimo(int numero, int divisorParaRecursividade)
+		{
+			if (numero == 1)
+			{
+				return false;
+			}
+			else if (Array.IndexOf(new int[] { 2, 3, 5, 7 }, numero) >= 0)
+			{
+				return true;
+			}
+			else if (EhDivisivelPorAlgumDosDivisores(numero, 2, 3, 5))
+			{
+				return false;
+			}
+			else
+			{
+				if (numero % divisorParaRecursividade == 0)
+				{
+					return false;
+				}
+				else
+				{
+					if (numero / divisorParaRecursividade > divisorParaRecursividade)
+					{
+						var proximoNumeroPrimo = ObterProximoNumeroPrimo(divisorParaRecursividade);
+
+						return NumeroEhPrimo(numero, proximoNumeroPrimo);
+					}
+					else
+					{
+						return true;
+					}
+				}
+			}
+		}
+
+		public static bool EhDivisivelPorAlgumDosDivisores(int numero, params int[] divisores)
+		{
+			foreach (int divisor in divisores)
+			{
+				if (numero % divisor == 0)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public static int ObterProximoNumeroPrimo(int numero)
+		{
+			do
+			{
+				numero++;
+			} while (!NumeroEhPrimo(numero, 7));
+
+			return numero;
+		}
+	}
+}
